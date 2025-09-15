94c96ce03312a16308e45610727bd041316d1ed9
Stanislav Rotar <rotarstanislav4@gmail.com>
2025-09-15 11:43:53 +0300

    Initial commit

---
+++ Lab1/Mood.cs
@@ -0,0 +1,7 @@
+﻿struct Mood
+{
+    public int RainyHome { get; set; }
+    public int SunnyHome { get; set; }
+    public int RainyForest { get; set; }
+    public int SunnyForest { get; set; }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿class Lab1 {
+    static void Main() {
+        int rainyHomeMood, sunnyHomeMood, rainyForestMood, sunnyForestMood;
+        double pRain;
+
+        MoodInput("How do you feel at home when it rains.Rate it on a 10-point scale", out rainyHomeMood);
+        MoodInput("How do you feel at home when the sun is shining.Rate it on a 10-point scale", out sunnyHomeMood);
+        MoodInput("How do you feel in the forest when it rains.Rate it on a 10-point scale", out rainyForestMood);
+        MoodInput("How do you feel in the forest when the sun is shining.Rate it on a 10-point scale", out sunnyForestMood);
+
+        Console.WriteLine("Enter the probability of rain");
+
+        while(true)
+        {
+            bool isTrue = double.TryParse(Console.ReadLine(), out pRain);
+
+            if(isTrue && pRain >= 0 && pRain <= 1)
+                break;
+
+            Console.WriteLine("You entered the probability of rain incorrectly.Try again");
+        }
+
+        double pSun = 1 - pRain;
+
+        double wHome = (pRain * rainyHomeMood + pSun * sunnyHomeMood);
+        double wForest = (pRain * rainyForestMood + pSun * sunnyForestMood);
+
+        Console.WriteLine("Benefit of the decision to stay at home: " + wHome);
+        Console.WriteLine("Benefit of the decision to go to the forest: " + wForest);
+
+        if(wHome >= wForest)
+            Console.WriteLine("Today it's better to stay at home!!");
+        else
+            Console.WriteLine("Today it's better to go to the forest!!");
+    }
+
+    static void MoodInput(string inputText, out int mood)
+    {
+        while (true)
+        {
+            Console.WriteLine(inputText);
+            bool isValid = int.TryParse(Console.ReadLine(), out mood);
+            if (isValid && mood >= 0 && mood <= 10)
+                break;
+            Console.WriteLine("You entered the number incorrectly. Try again.");
+        }
+    }
+}
